84d0bc08cc61d74948153d89dcbd8ef204685327
Ashok <ashok9597462162@gmail.com>
2020-12-09 20:03:22 +0530

    Commit from branch 1.

--- app/src/main/java/com/ezee/gitproject1/MainActivity.java
+++ app/src/main/java/com/ezee/gitproject1/MainActivity.java
@@ -3,6 +3,7 @@ package com.ezee.gitproject1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void branchOneMethod() {
+        Log.e("Branch", "branch 1 method");
     }
 }
